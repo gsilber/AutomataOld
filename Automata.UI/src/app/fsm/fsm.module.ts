f420af7995fd03c867d73c5841508363ab641081
Greg Silber <gsilber@cyberdaptive.com>
2018-04-17 22:30:02 -0400

    and more

--- Automata.UI/src/app/fsm/fsm.module.ts
+++ Automata.UI/src/app/fsm/fsm.module.ts
@@ -1,9 +1,13 @@
+import { FsmDrawModule } from './fsm-draw/fsm-draw.module';
+import { FsmCoreModule } from './fsm-core/fsm-core.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FsmCoreModule,
+    FsmDrawModule
   ],
   declarations: []
 })
