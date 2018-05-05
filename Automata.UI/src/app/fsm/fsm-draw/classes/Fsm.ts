3bb86efc80b55551a49d2086c1e3e196e9d7c137
Greg Silber <gsilber@cyberdaptive.com>
2018-05-05 18:05:08 -0400

    Added responsive features to left tab menu and ctrlbar

--- Automata.UI/src/app/fsm/fsm-draw/classes/Fsm.ts
+++ Automata.UI/src/app/fsm/fsm-draw/classes/Fsm.ts
@@ -2,11 +2,20 @@ export class FsmObject {
     type: string;
 }
 
+// States
+export class FsmStateData {
+
+}
 export class FsmState extends FsmObject {
+    private _data: FsmStateData;
+}
+
+// Transitions
+export class FsmTransitionData {
 
 }
 export class FsmTransition extends FsmObject {
-
+    private _data: FsmTransitionData;
 }
 
 export class Fsm {
