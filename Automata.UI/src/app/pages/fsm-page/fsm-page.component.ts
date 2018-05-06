e2f6a55bbee51b88749e4005bebdebe4131cdd21
Greg Silber <gsilber@cyberdaptive.com>
2018-05-05 20:29:30 -0400

    mobile version updates, now renders on both

--- Automata.UI/src/app/pages/fsm-page/fsm-page.component.ts
+++ Automata.UI/src/app/pages/fsm-page/fsm-page.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-fsm-page',
   templateUrl: './fsm-page.component.html',
   styleUrls: ['./fsm-page.component.scss']
 })
-export class FsmPageComponent implements OnInit {
+export class FsmPageComponent  {
+
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
