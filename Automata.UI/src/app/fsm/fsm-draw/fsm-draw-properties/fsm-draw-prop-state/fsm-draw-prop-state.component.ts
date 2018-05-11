b27a11ba84ee76ba30f804e6587746d6fd9ba00a
Greg Silber <gsilber@cyberdaptive.com>
2018-05-11 16:05:17 -0400

    hooked up input

--- Automata.UI/src/app/fsm/fsm-draw/fsm-draw-properties/fsm-draw-prop-state/fsm-draw-prop-state.component.ts
+++ Automata.UI/src/app/fsm/fsm-draw/fsm-draw-properties/fsm-draw-prop-state/fsm-draw-prop-state.component.ts
@@ -1,4 +1,4 @@
-import {  FsmState } from './../../classes/Fsm';
+import { FsmState } from './../../classes/Fsm';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -6,11 +6,46 @@ import { Component, Input } from '@angular/core';
   templateUrl: './fsm-draw-prop-state.component.html',
   styleUrls: ['./fsm-draw-prop-state.component.css']
 })
-export class FsmDrawPropStateComponent  {
+export class FsmDrawPropStateComponent {
 
-  @Input() state: FsmState;
+  @Input() set state(val: FsmState) {
+    this._state = val;
+    if (this._state) {
+      this.name = this._state.label;
+      this.start = this._state.start;
+      this.final = this._state.final;
+    } else {
+      this.name = ''; this.start = this.final = false;
+    }
+  }
+
+  get isValid() {
+    return true;
+  }
+  get isDirty() {
+    return (this.name !== this.state.label || this.start !== this.state.start || this.final !== this.state.final);
+  }
+  private _state: FsmState = null;
+
+  name: string;
+  start: boolean;
+  final: boolean;
+
+  errMsg = '';
 
   constructor() { }
 
+  onCancel() {
+    this.name = this._state.label;
+    this.start = this._state.start;
+    this.final = this._state.final;
+  }
+  onSubmit() {
+    if (this.isValid) {
+      this._state.label = this.name;
+      this._state.start = this.start;
+      this._state.final = this.final;
+    }
+  }
 
 }
